c4bcca47998152fdceefe241640fd901dd8e17ea
@askcasmir <casmirconsults@gmail.com>
2024-01-06 00:26:27 -0500

    WIP: Added Trapchain logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,49 +7,66 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useStore } from "zustand";
 
-
-
 export default function Home() {
   const theme = useStore(themeStore, (state) => state.theme);
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const { setTheme } = themeStore();
   const [showPassword, setShowPassword] = useState(false);
 
   useEffect(() => {
     console.log(theme);
-
-  }, [theme])
+  }, [theme]);
 
   function openModal() {
-    setIsOpen(true)
+    setIsOpen(true);
   }
-
 
   return (
     <>
-      <main className={`${theme === 'light' ? 'lightHeroBg' : 'darkHeroBg'} bg-center bg-cover bg-fixed`}>
-        <div className='md:px-20 px-8 space-y-4 md:space-y-0 md:flex justify-between items-center py-12 w-full fixed'>
-          <Link href={'/'} className='relative w-32 hover:scale-[0.99] duration-200 transition'>
-            <img src={'/images/logo_l.svg'} alt='logo' />
+      <main
+        className={`${
+          theme === "light" ? "lightHeroBg" : "darkHeroBg"
+        } bg-center bg-cover bg-fixed`}
+      >
+        <div className="md:px-20 px-8 space-y-4 md:space-y-0 md:flex justify-between items-center py-12 w-full fixed">
+          <Link
+            href={"/"}
+            className="relative w-32 hover:scale-[0.99] duration-200 transition"
+          >
+            <img src={"/images/TrapCard.png"} alt="logo" />
           </Link>
-          <div className='flex gap-x-8 items-center'>
-            <button onClick={() => {
-              setTheme(theme === 'dark' ? 'light' : 'dark');
-            }}>
+          <div className="flex gap-x-8 items-center">
+            <button
+              onClick={() => {
+                setTheme(theme === "dark" ? "light" : "dark");
+              }}
+            >
               <FontAwesomeIcon
                 icon={faMoon}
-                className={'h-6 w-6 text-teal-500'}
+                className={"h-6 w-6 text-teal-500"}
               />
             </button>
-            <button className='rounded-xl py-2.5 font-semibold bg-white px-5 hover:scale-[0.99] duration-200 transition'>Claim Drop</button>
-            <button className='rounded-xl py-2.5 font-semibold greenToPurple px-3 hover:scale-[0.99] duration-200 transition' onClick={openModal}>Login</button>
+            <button className="rounded-xl py-2.5 font-semibold bg-white px-5 hover:scale-[0.99] duration-200 transition">
+              Claim Drop
+            </button>
+            <button
+              className="rounded-xl py-2.5 font-semibold greenToPurple px-3 hover:scale-[0.99] duration-200 transition"
+              onClick={openModal}
+            >
+              Login
+            </button>
           </div>
         </div>
         <div className="md:px-32 px-8 space-y-8 md:pt-40 pt-48">
           <p className="font-extralight dark:text-white">
-            360NFT is currently invite-only
+            TrapCard is currently invite-only
             <br />
-            Here&apos;s how to <span><Link href={'/'} className="text-cyanSky">skip the waitlist now</Link></span>
+            Here&apos;s how to{" "}
+            <span>
+              <Link href={"/"} className="text-cyanSky">
+                skip the waitlist now
+              </Link>
+            </span>
           </p>
           <h1 className="text-5xl font-druk text-corbeau/80">
             GET ON
@@ -63,16 +80,28 @@ export default function Home() {
           </h3>
           <div className="rounded-3xl shadow-lg greenToPurple p-6 max-w-xl ">
             <h3 className="text-4xl font-black mb-5">Get early access</h3>
-            <input type="email" name="" id="" className="bg-white py-2.5 rounded-lg px-4 w-full mb-5" placeholder="Email" />
+            <input
+              type="email"
+              name=""
+              id=""
+              className="bg-white py-2.5 rounded-lg px-4 w-full mb-5"
+              placeholder="Email"
+            />
             <div className="w-full">
-              <button className="bg-black w-full text-sm py-2 rounded-lg">Register</button>
-              <button className="text-gray-300 text-sm mt-4">Signed Up before? Check your status</button>
+              <button className="bg-black w-full text-sm py-2 rounded-lg">
+                Register
+              </button>
+              <button className="text-gray-300 text-sm mt-4">
+                Signed Up before? Check your status
+              </button>
             </div>
           </div>
         </div>
         <footer className="md:px-40 px-10 flex justify-between text-sm mt-20 pb-8">
-          <p>© 2021 360NFT</p>
-          <Link href={'/'} className="text-blue-500 hover:text-blue-600">Terms & conditions</Link>
+          <p>© 2024 TrapCard™️</p>
+          <Link href={"/"} className="text-blue-500 hover:text-blue-600">
+            Terms & conditions
+          </Link>
         </footer>
       </main>
       <Modal openModal={isOpen} closeModal={() => setIsOpen(false)}>
@@ -80,38 +109,71 @@ export default function Home() {
           <div className=" w-1/2">
             <h2 className="text-4xl mt-4">Login</h2>
             <div className="flex flex-col gap-y-1 my-4">
-              <label htmlFor="" className="text-xs text-cyan-400">Email</label>
-              <input type="email" name="" id="" className="py-2 px-4 bg-white rounded-lg text-black" placeholder="Email" />
+              <label htmlFor="" className="text-xs text-cyan-400">
+                Email
+              </label>
+              <input
+                type="email"
+                name=""
+                id=""
+                className="py-2 px-4 bg-white rounded-lg text-black"
+                placeholder="Email"
+              />
             </div>
             <div className="flex flex-col gap-y-1">
-              <label htmlFor="" className="text-xs text-cyan-400">Password</label>
+              <label htmlFor="" className="text-xs text-cyan-400">
+                Password
+              </label>
               <div className="w-full bg-white rounded-lg overflow-hidden flex items-center">
-                <input type={showPassword ? 'text' : 'password'} name="" id="" className="py-2 border-r outline-none px-4  text-black" placeholder="Password" />
-                <button onClick={() => setShowPassword(!showPassword)} className="flex justify-center w-full">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name=""
+                  id=""
+                  className="py-2 border-r outline-none px-4  text-black"
+                  placeholder="Password"
+                />
+                <button
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="flex justify-center w-full"
+                >
                   <FontAwesomeIcon
                     icon={showPassword ? faEyeSlash : faEye}
-                    className={'h-4 w-4 text-black'}
+                    className={"h-4 w-4 text-black"}
                   />
                 </button>
               </div>
             </div>
             <button className="text-xs text-gray-300">Forgot password?</button>
             <div className="mt-6">
-              <button className="greenToPurple py-2 px-6 rounded-xl">Log in</button>
+              <button className="greenToPurple py-2 px-6 rounded-xl">
+                Log in
+              </button>
             </div>
           </div>
           <div className="w-1/2">
             <h3 className="font-semibold mt-4 text-xl">Share the love</h3>
-            <p className="text-sm mt-6 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus facere delectus eveniet voluptate adipisci voluptates ut?</p>
+            <p className="text-sm mt-6 mb-5">
+              Trap Card Beta program: $250 a year early adopters Your referral
+              code discount $99/year for both You and your referral!
+            </p>
             <div>
               <div className="flex flex-col gap-y-1 mb-4">
-                <label htmlFor="" className="text-xs text-cyan-400">Referral Url</label>
+                <label htmlFor="" className="text-xs text-cyan-400">
+                  Referral Url
+                </label>
                 <div className="w-full bg-white rounded-lg overflow-hidden flex items-center">
-                  <input type={'text'} name="" id="" value={'360nft.app/er23rx'} className="py-2 flex-1 border-r outline-none px-4 text-black" placeholder="Password" />
+                  <input
+                    type={"text"}
+                    name=""
+                    id=""
+                    value={"360nft.app/er23rx"}
+                    className="py-2 flex-1 border-r outline-none px-4 text-black"
+                    placeholder="Password"
+                  />
                   <button className="flex justify-center w-10">
                     <FontAwesomeIcon
                       icon={faCopy}
-                      className={'h-4 w-4 text-black'}
+                      className={"h-4 w-4 text-black"}
                     />
                   </button>
                 </div>
@@ -121,5 +183,5 @@ export default function Home() {
         </div>
       </Modal>
     </>
-  )
+  );
 }
